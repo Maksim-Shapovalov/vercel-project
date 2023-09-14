5f5663998a7161b92f3c0d00dcc7533e6839f376
Maksim Shapovalov <maksim.shapovalov.01@gmail.com>
2023-09-14 11:28:43 +0300

    Add project

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const products = [{id:1,name:'tomato'},{id:2,name:'potato'}]
 app.get('/', (req: Request, res: Response) =>{
     res.send('Hello Samurai')
 })
-app.get('/', (req: Request, res: Response) =>{
+app.get('/products', (req: Request, res: Response) =>{
     for (let i = 0; i < products.length; i++){
         if (products){
             res.send(products[i])
